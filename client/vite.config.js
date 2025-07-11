14fee2a7db5e7142149f9cd79c75ad39b9b49229
daidaiminah <daidaiminahtswist@gmail.com>
2025-07-11 13:53:27 +0000

    edit the links for the bacend

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        // target: 'http://localhost:3001',
+        target: 'https://whip-in-time-server.onrender.com',
         changeOrigin: true,
         secure: false
       }
